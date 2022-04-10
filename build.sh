b3dcfcc4daac09d29aa2e7e2b119dd099db105fd
camjac251 <camjac251@users.noreply.github.com>
2022-04-10 11:44:03 -0500

    update build and deploy script

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
 STYLE="\e[36;1m\e[1m"
 RESET="\e[0m"
 
 TYPE=${TYPE:-Release}
 
+rm -rf release
 mkdir -p release
-cd release
+cd release || exit
 
-echo -e "${STYLE}Compiling moonjit...${RESET}"
-pushd ../moonjit/src
-	make XCFLAGS+=-DLUAJIT_ENABLE_LUA52COMPAT -j${nproc}
-popd
+echo -e "${STYLE}Compiling (cmake) moonjit...${RESET}"
+pushd ../moonjit/src || exit
+make clean
+make XCFLAGS+=-DLUAJIT_ENABLE_LUA52COMPAT -j"${nproc}"
+popd || exit
 
-echo -e "${STYLE}Compiling RosaServer (${TYPE})...${RESET}"
-cmake -DCMAKE_BUILD_TYPE=${TYPE} ..
-make -j${nproc}
+echo -e "${STYLE}Compiling (cmake) RosaServer (${TYPE})...${RESET}"
+cmake -DCMAKE_BUILD_TYPE="${TYPE}" ..
+echo -e "${STYLE}Compiling (make) RosaServer (${TYPE})...${RESET}"
+make -j"${nproc}"
