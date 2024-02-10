563c4b8523dc05bc64de6739179daf44d130d869
checkraisefold <checkraisefold@crf.gay>
2024-02-10 05:15:22 -0800

    buildscript changes

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ cd release || exit
 echo -e "${STYLE}Compiling moonjit...${RESET}"
 pushd ../moonjit/src || exit
 make clean
-make XCFLAGS+=-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64 -j"${nproc}"
+make XCFLAGS+="-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64 -j${nproc}"
 popd || exit
 
 echo -e "${STYLE}Generating build files with CMake (${TYPE})...${RESET}"
 cmake -DCMAKE_BUILD_TYPE="${TYPE}" ..
 echo -e "${STYLE}Compiling RosaServer (${TYPE})...${RESET}"
-make -j"${nproc}"
+cmake --build .. --config $BUILD_TYPE --parallel "${nproc}"
