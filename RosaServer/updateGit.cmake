55f29aa94a3de33f72c2d3aeebcae1469037a82b
checkraisefold <checkraisefold@crf.gay>
2024-02-16 00:04:25 -0800

    embed the git sha in the binary

---
+++ RosaServer/updateGit.cmake
@@ -0,0 +1,7 @@
+execute_process (
+    COMMAND git log -1 --format=%h
+    WORKING_DIRECTORY ${GIT_DIR}
+    OUTPUT_VARIABLE GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+configure_file(${PRE_CONFIGURE_FILE} ${POST_CONFIGURE_FILE} @ONLY)
