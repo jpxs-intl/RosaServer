7f75ddc305f843eac7e693daffeb7c3842edb087
checkraisefold <checkraisefold@crf.gay>
2024-04-08 19:51:49 -0700

    improve buildscript for new cmake versions

--- build.sh
+++ build.sh
@@ -4,17 +4,13 @@ RESET="\e[0m"
 
 TYPE=${TYPE:-Release}
 
-rm -rf release
-mkdir -p release
-cd release || exit
-
 echo -e "${STYLE}Compiling moonjit...${RESET}"
-pushd ../moonjit/src || exit
+pushd ./moonjit/src || exit
 make clean
 make -j"${nproc}" XCFLAGS+="-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64"
 popd || exit
 
 echo -e "${STYLE}Generating build files with CMake (${TYPE})...${RESET}"
-cmake -DCMAKE_BUILD_TYPE="${TYPE}" ..
+cmake -DCMAKE_BUILD_TYPE="${TYPE}" .
 echo -e "${STYLE}Compiling RosaServer (${TYPE})...${RESET}"
 cmake --build . --config "${TYPE}" --parallel "${nproc}"
